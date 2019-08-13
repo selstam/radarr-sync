aa8404e989bd70bcc7fd38c2395ca25adc8e849b
Andreas Selstam <andreas@selstam.nu>
2019-08-13 14:25:48 +0200

    Add project files.

---
+++ selstam-radarr-sync/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace selstam_radarr_sync
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
